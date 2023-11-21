8e8fb41623ac7a5e8b90d8b02dd0a96120c04aaa
weijiaduo <648620282@qq.com>
2023-11-21 23:39:32 +0800

    fix: 字符串匹配-RK 算法

--- algorithm/src/main/java/com/wjd/algorithm/strings/search/BruteForceSearch.java
+++ algorithm/src/main/java/com/wjd/algorithm/strings/search/BruteForceSearch.java
@@ -2,6 +2,12 @@ package com.wjd.algorithm.strings.search;
 
 /**
  * 暴力搜索法
+ * <p>
+ * 检查主串的所有子串，看是否和模式串匹配
+ * <p>
+ * 以每个字符为起点开始匹配，一旦匹配失败，就重头开始
+ * <p>
+ * 复杂度：时间 O(mn) 空间 O(1)
  *
  * @author weijiaduo
  * @since 2023/3/28
--- algorithm/src/main/java/com/wjd/algorithm/strings/search/RabinKarpSearch.java
+++ algorithm/src/main/java/com/wjd/algorithm/strings/search/RabinKarpSearch.java
@@ -4,7 +4,9 @@ import java.math.BigInteger;
 import java.util.Random;
 
 /**
- * Rabin Karp 算法
+ * Rabin Karp 算法，又称指纹字符串查找算法
+ * <p>
+ * 复杂度：最好时间 O(n) 最坏时间 O(mn) 空间 O(1)
  *
  * @author weijiaduo
  * @since 2023/3/30
@@ -22,41 +24,32 @@ public class RabinKarpSearch implements Search {
 
     @Override
     public int search(String pat, String txt) {
-        mod = longRandomPrime();
         int m = pat.length(), n = txt.length();
-        long rm = 1;
-        for (int i = 1; i < m; i++) {
+        if (m == 0 || n < m) {
+            return -1;
+        }
+
+        mod = longRandomPrime();
+        long phash = 0, thash = 0, rm = 1;
+        for (int i = 0; i < m - 1; i++) {
+            phash = (R * phash + pat.charAt(i)) % mod;
+            thash = (R * thash + txt.charAt(i)) % mod;
+            // 首字符的幂值 R^(M-1)
             rm = (R * rm) % mod;
         }
-        long phash = hash(pat, m);
-        long thash = hash(txt, m);
-        if (phash == thash && check(pat, txt, 0, m)) {
-            return 0;
-        }
-        for (int i = 1; i <= n - m; i++) {
-            thash = (thash - txt.charAt(i - 1) * rm + mod) % mod;
-            thash = (R * thash + txt.charAt(i + m - 1)) % mod;
-            if (phash == thash && check(pat, txt, i, m)) {
-                return i;
+        phash = (R * phash + pat.charAt(m - 1)) % mod;
+
+        for (int i = m - 1, j = 0; i < n; i++, j++) {
+            // 追加新字符的哈希值
+            thash = (R * thash + txt.charAt(i)) % mod;
+            // 验证子串[j, i]与模式串是否匹配
+            if (phash == thash && check(pat, txt, j, m)) {
+                return j;
             }
+            // 减去首字符的哈希值
+            thash = (thash - txt.charAt(j) * rm + mod) % mod;
         }
         return -1;
-    }
-
-    /**
-     * 计算哈希值
-     *
-     * @param s 字符串
-     * @param n 长度
-     * @return 哈希值
-     */
-    private long hash(String s, int n) {
-        long h = 0;
-        for (int i = 0; i < n; i++) {
-            h = h * R + s.charAt(i);
-            h %= mod;
-        }
-        return h;
     }
 
     /**
@@ -64,13 +57,13 @@ public class RabinKarpSearch implements Search {
      *
      * @param pat    模式串
      * @param txt    主串
-     * @param index  主串起始索引
+     * @param offset 主串起始索引
      * @param length 长度
      * @return true/false
      */
-    private boolean check(String pat, String txt, int index, int length) {
+    private boolean check(String pat, String txt, int offset, int length) {
         for (int i = 0; i < length; i++) {
-            if (pat.charAt(i) != txt.charAt(index + i)) {
+            if (pat.charAt(i) != txt.charAt(offset + i)) {
                 return false;
             }
         }
